6566b7e8683be22859ef13e421293ef10c054fa9
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2026-06-26 14:35:22 -0300

    Ajustando para rodas o projeto no IOS.

--- composeApp/src/iosMain/kotlin/br/com/correios/ppm/di/KoinInitialize.kt
+++ composeApp/src/iosMain/kotlin/br/com/correios/ppm/di/KoinInitialize.kt
@@ -1,9 +1,18 @@
 package br.com.correios.ppm.di
 
+import br.com.correios.ppm.login.data.LoginService
+import br.com.correios.ppm.unidade.data.UnidadeService
 import org.koin.core.context.startKoin
+import org.koin.dsl.module
 
-fun initKoin(){
-    val modules = sharedModules + databaseModule
+private const val IOS_BASE_URL = "https://applogisticahom.correios.com.br"
+
+fun initKoin() {
+    val iosModule = module {
+        single { LoginService(get(), IOS_BASE_URL) }
+        single { UnidadeService(get(), IOS_BASE_URL) }
+    }
+    val modules = sharedModules + databaseModule + iosModule
 
     startKoin {
         modules(modules)
