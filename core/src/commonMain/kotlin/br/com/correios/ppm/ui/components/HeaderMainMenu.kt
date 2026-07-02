5501ae0fc0fede8cb85858ddeddc2c269db023aa
Rafael Valentim Fonseca <rvalentim@correios.com.br>
2026-07-02 16:53:36 -0300

    Criando o módulo de concursos e migrando o menu principal para o KMP.

--- core/src/commonMain/kotlin/br/com/correios/ppm/ui/components/HeaderMainMenu.kt
+++ core/src/commonMain/kotlin/br/com/correios/ppm/ui/components/HeaderMainMenu.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
@@ -75,6 +76,7 @@ fun HeaderMainMenu(
     Column(
         modifier = Modifier
             .fillMaxWidth()
+            .statusBarsPadding()
             .padding(8.dp),
         verticalArrangement = Arrangement.spacedBy(8.dp)
     ) {
